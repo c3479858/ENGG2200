3c255552c42c68024631e9aa37e7c77d4c3c6fb2
c3479858 <c3479858@uon.edu.au>
2026-09-01 23:16:38 +1000

    Update capstone.ino

--- ZtH/CAN Bus/capstone/capstone.ino
+++ ZtH/CAN Bus/capstone/capstone.ino
@@ -15,10 +15,10 @@ int S = 0;
 
 void setup() {
   can1.begin();
-  can1.setBaudRate(500000); //kb/s
-  Serial.begin(9600);       //start serial
+  can1.setBaudRate(500000);  //kb/s
+  Serial.begin(9600);        //start serial
 
-  outgoing_msg.id = 0;            // Message ID. Decimal, 0-2047
+  outgoing_msg.id = 0;              // Message ID. Decimal, 0-2047
   outgoing_msg.flags.remote = 0;    // RTR - 0 for standard data frames
   outgoing_msg.flags.extended = 0;  // IDE - 0 for standard 11-bit IDs
   outgoing_msg.flags.reserved = 0;  // Reserved bit. Leave as dominant (0)
@@ -27,8 +27,8 @@ void setup() {
 }
 
 void loop() {
-  msg_read = can1.read(incoming_msg);       //sets msg_read to true (1) if message has been read
-  if (msg_read){
+  msg_read = can1.read(incoming_msg);  //sets msg_read to true (1) if message has been read
+  if (msg_read) {
     //computes solution for S according to capstone details
     x = (0.1 * incoming_msg.buf[0]) + 3;
     y = (0.2 * incoming_msg.buf[1]) + 2;
@@ -38,10 +38,6 @@ void loop() {
     //sets outgoing message to S, with scaling factor 1 and no offset
     outgoing_msg.buf[0] = S;
     can1.write(outgoing_msg);
-    Serial.println("Message sent!")
-    //mostly for debugging, but helpful to see on serial
-  }else{
-    Serial.println("Waiting for message...")
-    delay(1000)
+    Serial.println("Message sent!");
   }
 }
